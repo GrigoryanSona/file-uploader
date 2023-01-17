e524c3fef1c743d26a3ddd7eaaafb930b9ccf8a4
Sona Grigoryan <sgrigoryan@asdco.ru>
2023-01-17 20:19:17 +0400

    File uploader

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'files-upload1';
+
+  files: any[] = [];
+
+  fileContent: any
+
+  onFileDropped($event: any) {
+    this.prepareFilesList($event);
+  }
+
+  fileBrowseHandler(files: any) {
+    this.prepareFilesList(files.target.files);
+  }
+
+  deleteFile(index: number) {
+    this.files.splice(index, 1);
+  }
+
+  uploadFilesSimulator(index: number) {
+    setTimeout(() => {
+      if (index === this.files.length) {
+        return;
+      } else {
+        const progressInterval = setInterval(() => {
+          if (this.files[index].progress === 100) {
+            clearInterval(progressInterval);
+            this.uploadFilesSimulator(index + 1);
+          } else {
+            this.files[index].progress += 5;
+          }
+        }, 200);
+      }
+    }, 1000);
+  }
+
+  prepareFilesList(files: Array<any>) {
+    for (const item of files) {
+      item.progress = 0;
+      this.files.push(item);
+    }
+    this.uploadFilesSimulator(0);
+  }
+
+ 
+  readDocument(file: any) {
+    let fileReader: FileReader = new FileReader();
+    let self = this;
+    fileReader.onload = () => {
+      self.fileContent = fileReader.result;
+    }
+    fileReader.readAsText(file);
+    console.log(this.fileContent)
+  }
+  formatBytes(bytes: any, decimals?: any) {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+}
